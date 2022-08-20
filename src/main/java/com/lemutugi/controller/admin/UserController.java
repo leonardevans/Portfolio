40e0cda6863665a74ade983f7377408df696cc52
leonardevans <lemutugi@gmail.com>
2022-08-20 09:12:07 -0400

    showUsers method in user controller

--- src/main/java/com/lemutugi/controller/admin/UserController.java
+++ src/main/java/com/lemutugi/controller/admin/UserController.java
@@ -1,12 +1,59 @@
 package com.lemutugi.controller.admin;
 
+import com.lemutugi.model.Role;
+import com.lemutugi.model.User;
+import com.lemutugi.repository.RoleRepository;
+import com.lemutugi.service.UserService;
+import com.lemutugi.utils.Constants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.security.RolesAllowed;
+import java.util.List;
 
 @RequestMapping("/admin/users/")
 @RolesAllowed("ROLE_ADMIN")
 @Controller
 public class UserController {
+    private UserService userService;
+    private RoleRepository roleRepository;
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserController(UserService userService, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
+        this.userService = userService;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping(value = {"/list", "/"})
+    public String showUsers(
+            @RequestParam(defaultValue = "1", required = false) int pageNo,
+            @RequestParam(defaultValue = Constants.SMALL_PAGE_SIZE, required = false) int pageSize,
+            @RequestParam(defaultValue = "name", required = false) String sortField,
+            @RequestParam(defaultValue = "asc", required = false) String sortDir ,
+            Model model){
+
+        Page<User> userPage = userService.getAll(pageNo, pageSize, sortField, sortDir);
+        List<User> users = userPage.getContent();
+
+        model.addAttribute("pageSize", pageSize);
+        model.addAttribute("currentPage", pageNo);
+        model.addAttribute("totalPages", userPage.getTotalPages());
+        model.addAttribute("totalItems", userPage.getTotalElements());
+
+        model.addAttribute("sortField", sortField);
+        model.addAttribute("sortDir", sortDir);
+        model.addAttribute("reverseSortDir", sortDir.equals("asc") ? "desc" : "asc");
+
+        model.addAttribute("users", users);
+        return "/admin/users";
+    }
+
 }
