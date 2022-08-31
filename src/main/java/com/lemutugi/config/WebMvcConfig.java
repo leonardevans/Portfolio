9a3849a4eac37130ca502f44a57c4def4d451e95
leonardevans <lemutugi@gmail.com>
2022-08-31 16:14:14 -0400

    solved cors error

--- src/main/java/com/lemutugi/config/WebMvcConfig.java
+++ src/main/java/com/lemutugi/config/WebMvcConfig.java
@@ -15,7 +15,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         long MAX_AGE_SECS = 3600;
         registry.addMapping("/**")
-                .allowedOrigins(baseUrl)
+                .allowedOrigins(baseUrl, "https://lemutugi.herokuapp.com/auth/login")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
